7bc12da2a7a0faee385d5d5c145c2e534da1f000
David Belfort <149313325+dandat309@users.noreply.github.com>
2025-06-26 16:38:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ toggle.addEventListener('click', () => {
   document.body.classList.toggle('light');
   document.body.classList.toggle('dark');
 });
+
+function criarCard(titulo, imagem) {
+  return { titulo,imagem };
+}
+
+let cardsGaleria = [
+  criarCard(
+    'Conceito',
+    'ideia1.jpg'
+  ),
+  criarCard(
+    'Programação',
+    '_MG_6654.JPG'
+  ),
+  criarCard(
+    'Artes e Design',
+    'boneco peso.jpg'
+  )
+];
+document.addEventListener('DOMContentLoaded', () => {
+  const figures = document.querySelectorAll('#desenvolvimento .galeria figure ');
+  for (let i = 0; i < cardsGaleria.length && i < figures.length; i++) {
+    const figure = figures[i];
+    const img = figure.querySelector('img');
+    img.src = cardsGaleria[i].imagem;
+    img.alt = cardsGaleria[i].titulo;
+  }
+});
